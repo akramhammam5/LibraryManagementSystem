393852883b1ffe2f2943d3a822d72f161f5ebaa4
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2023-04-24 14:38:12 +0200

    Made changes

--- Librarian.java
+++ Librarian.java
@@ -1,38 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.laibraryproject;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.*;
 
-import java.lang.reflect.Member;
-import java.util.Scanner;
-
-
-public class Librarian {
-
-    
-
-        public String name;
-        private int ID;
-    
-        public void Welcome()
-        {
-            Scanner scan = new Scanner(System.in);
-            System.out.print("\u001b[2J");
-            System.out.flush();
-            System.out.println("\t\t              Welcome To LibraryManagementSystem :)           \n\n");
-            System.out.println("you are a Librarian What do you'd like to do today :) ?");
-            System.out.println("1- Add User\n2- Add Books\n3- Logout :(\n");
-            int choice = scan.nextShort();
+public class Libraria extends Registeration {
+    public static void Welcome() throws FileNotFoundException{
+        try {
+             
+            File obj = new File("Readers.txt");
+             
+            if (obj.createNewFile()) 
+            {
+              System.out.println("File is created");
+            } 
+             
+          } 
+          catch (IOException e) 
+          {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+             
+          }
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\u001b[2J");
+        System.out.flush();
+        System.out.println("\t\t              Welcome To LibraryManagementSystem :)           \n\n");
+        System.out.println("you are a Librarian What do you'd like to do today :) ?");
+        System.out.println("1- Add User\n2- Add Books\n3-Search for books\n4-Search for a member\n5-Block a user\n6-Rent a book\n6-Logout\n");
+        System.out.println("Choice: ");
+        int choice = scan.nextShort();
 
         switch(choice){
-            case 3:
-            { 
+            case 1:
+            {
+                System.out.print("\u001b[2J");
+                System.out.flush();
+                Libraria.adduser();
+                break;
+            }
+            case 6:{ 
             System.out.print("\u001b[2J");
             System.out.flush();
             System.out.println("Logged Out!\n\n\n");
             Registeration R = new Registeration();
-            R.register();
+            R.login();
             break;
             }
             default:
@@ -40,71 +53,60 @@ public class Librarian {
                 System.out.println("Invalid Choice");
             }
         }
-            
-        
-        }
-        public void AddUser(Scanner sc){
-            System.out.println("Enter Your Name");
-            String title=sc.next();
-            int j = 0;
-            User user =new User(title,j+1);
-            User[] users = null;
-            users[j]=user;
-            j++;
-            System.out.println("User Added ID:"+users[j-1].getID());       
-    
+        //scan.close();
     }
-    public void RemoveUser(int ID,String username){
-        
     
-    }
-    public void Block(){
-    
-    
-    }
-    public String SearchUser(){
-        
-        
-        return null;
-        
-        
-    }
-   public String Rent(){
-      
-       
-       
-       
-        return null;
-       
-       
-   }
-   public String PrintBill(Scanner sc){
-       /*int fine;
-       int bill=100;
-       System.out.println("Enter Member ID");
-       int id=sc.nextInt();
-        members[] member=new members[id-1];
-        if(member.getIssued)>0){
-           Book[] books = null;
-            Book book=books[member.getIssued()];
-            fine=new Fine(book.getIssuedDte(),book.getReturnDate()).getFine();
-           
-   }else{
-            fine=0;
-        }
-        bill+=fine;
-        System.out.println("Bill Amount is:"+bill);
-        return null;
-        
-               
-   }*/
+    public static void adduser() throws FileNotFoundException
+    {
+        String name;
+        String pass;
+        int id;
+        String conpass;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter User's Name: ");
+        name = s.nextLine();
+        System.out.println("Enter User's Password: ");
+        pass = s.nextLine();
+        System.out.println("Confirm User's Password: ");
+        conpass=s.nextLine();
+        System.out.println("Enter User's ID: ");
+        id = s.nextInt();
 
-    private static class members {
+        name=name.trim();
+        pass=pass.trim();
+        String x=name+" "+pass+" "+id;
 
-        public members() {
+        if(pass.equals(conpass))
+        {
+             
+              File f = new File("Readers.txt");
+              Scanner file = new Scanner(f);
+              int flag=0;
+              while (file.hasNextLine()) {
+                String data = file.nextLine();
+                if(data.equals(x))
+                {
+                    System.out.println("Already Registered");
+                    flag=1;
+                }
+            }
+              if(flag==0)
+              {
+                  try {
+                      BufferedWriter out = new BufferedWriter(new FileWriter("Readers.txt", true)); 
+                      out.write(name+" "+pass+" "+id+"\n");
+                      out.close();
+                  }
+                  catch (IOException e) {
+                      System.out.println("exception occoured" + e);
+                  }
+                   
+                  System.out.println("Successfully Registered");
+                  
+                }
+                file.close();
         }
+       // s.close();
     }
-           
-   }
+    
 }
-
